85c088a7ec9e783b5ab7462b4202ba5e8a54e949
Joe Blau <im@joeblau.com>
2020-01-08 06:37:59 -0800

    Add basic hero scaffold

--- Sources/Newsshield/main.swift
+++ Sources/Newsshield/main.swift
@@ -2,6 +2,18 @@ import Foundation
 import Publish
 import Plot
 
+enum DownloadState {
+    case download
+    case preOrder
+}
+
+struct Download {
+    let title = "Download"
+    let subtitle = "Get News Shield for Safari on macOS"
+    let appStoreURL = "https://apps.apple.com/us/app/news-shield/id1489025442?ls=1&mt=12"
+    let state: DownloadState = .download
+}
+
 // This type acts as the configuration for your website.
 struct Newsshield: Website {
     enum SectionID: String, WebsiteSectionID {
@@ -21,7 +33,8 @@ struct Newsshield: Website {
     var imagePath: Path? { nil }
     var keywords = "Redact, Sensational, News, Shield, Desational"
     var copyright = "Copyright © 2019 - 2020 Get Shields"
+    var download = Download()
 }
 
 // This will generate your website using the built-in Foundation theme:
-try Newsshield().publish(withTheme: .foundation)
+try Newsshield().publish(withTheme: .stevenPaulJobs)
